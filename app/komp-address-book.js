5606af9bb134f8daf32c9d5aa96362dcb9864b12
Fernando Gabrieli <fgabrieli@gmail.com>
2016-05-09 02:07:18 -0300

    Adding kIf with function calls. Scope uses now prototype inheritance.

--- app/komp-address-book.js
+++ app/komp-address-book.js
@@ -4,7 +4,13 @@ app.komp.AddressBook = {
     templateUrl : 'komp-address-book.html',
     
     numbers : [2, 3, 4, 5, 6],
+    
+    show : true,
 
+    isEven : function(n) {
+        return n % 2 == 0;
+    },
+    
     init : function() {
         console.log('i was initialized');
     }
--- core.js
+++ core.js
@@ -26,90 +26,10 @@ window.komp = {
 
         var t = this;
         $.when(this.setupTemplate()).then(function() {
+            var compiler = new komp.Compiler();
             var rootScope = new komp.Scope(t.controller);
-            t.compile(t.$componentEl, rootScope);
+            compiler.compile(t.$componentEl, rootScope);
         })
-    },
-
-    compile : function($el, scope) {
-        this.eval($el, scope);
-
-        var t = this;
-        $el.children().each(function(i, el) {
-            t.compile($(el), scope);
-        })
-    },
-
-    eval : function($el, scope) {
-        var t = this;
-
-        if (hasFor($el) && !isForCompiled($el)) {
-            console.log('evaluating', $el, 'scope: ', scope);
-
-            var newScope = new komp.Scope(scope);
-
-            var expr = $el.attr('kfor');
-            var parsedFor = expr.match(/(.*)\s+?in\s+?(.*)/);
-            var indexName = parsedFor[1];
-            var sourceName = parsedFor[2];
-            var items = scope.getSymbol(sourceName);
-
-            applyFor.call(this, indexName, items, $el, scope);
-
-            return;
-        }
-
-        if (hasVariables($el)) {
-            var html = elToHtml($el);
-            var variables = html.match(/{(.*?)}/g);
-
-            var replaced = false;
-
-            for (var i = 0; i < variables.length; i++) {
-                value = scope.getSymbol(variables[i].replace(/{|}/g, ''));
-                if (value) {
-                    html = html.replace(variables[i], value);
-                    replaced = true;
-                }
-            }
-
-            if (replaced)
-                $el.replaceWith($(html));
-        }
-
-        function hasVariables($el) {
-            var html = elToHtml($el);
-            return /{.*}/.test(html);
-        }
-
-        function elToHtml($el) {
-            return $('<div>').append($el.clone()).html();
-        }
-
-        function hasFor($el) {
-            return $el[0].hasAttribute('kFor');
-        }
-
-        function isForCompiled($el) {
-            return $el[0].hasAttribute('kFor-compiled');
-        }
-
-        function applyFor(indexName, items, $el, scope) {
-            var newScope = new komp.Scope(scope);
-
-            $el.attr('kFor-compiled', true);
-
-            for (var i = 0; i < items.length; i++) {
-                newScope.setSymbol(indexName, items[i]);
-
-                var $newEl = $el.clone();
-                $newEl.insertBefore($el);
-
-                this.eval($newEl, newScope);
-            }
-
-            $el.remove();
-        }
     },
 
     setupController : function() {
@@ -155,22 +75,172 @@ window.komp = {
 }
 
 komp.Scope = function(parentScope) {
-    var symbols = {};
+    var scope = function() {
+    }
 
-    this.setSymbol = function(symbol, value) {
-        symbols[symbol] = value;
-    },
+    scope.prototype = Object.create(parentScope);
 
-    this.getSymbol = function(symbol) {
-        if (symbols[symbol])
-            return symbols[symbol];
+    return new scope();
+}
 
-        if (parentScope) {
-            if (parentScope.getSymbol) {
-                return parentScope.get(symbol);
+komp.Compiler = function() {
+    this.compile = function($el, scope) {
+        this.eval($el, scope);
+
+        var t = this;
+        $el.children().each(function(i, el) {
+            t.compile($(el), scope);
+        })
+    }
+
+    this.eval = function($el, scope) {
+        var t = this;
+
+        // console.log('evaluating', $el.html(), 'scope: ', scope);
+
+        if (hasFor($el) && !isForCompiled($el)) {
+
+            var newScope = new komp.Scope(scope);
+
+            var expr = $el.attr('kfor');
+            var parsedFor = expr.match(/(.*)\s+?in\s+?(.*)/);
+            var indexName = parsedFor[1];
+            var sourceName = parsedFor[2];
+            var items = scope[sourceName];
+
+            applyFor.call(this, indexName, items, $el, scope);
+        }
+
+        if (hasVariables($el)) {
+            $el = setVariables($el, scope);
+        }
+
+        if (hasIf($el)) {
+            var condition = $el.attr('kIf');
+            
+            var parts = condition.match(/(.*?)\s+?([=|<|>|!|>=|<=|]+)\s+?(.*)/);
+            if (parts == null) {
+                evalProposition(condition);
             } else {
-                return parentScope[symbol]; // root scope
+                var conditionType = parts[2];
+                
+                evalProposition(parts[1]);
+                
+                evalProposition(parts[3]);
+            }
+
+            function evalProposition(prop) {
+                if (!isLiteral(prop)) {
+                    condition = condition.replace(prop, 'this.' + prop);
+                }
+                
+                if (isFunction(prop)) {
+                    var params = prop.match(/(.*)\((.*)+\)$/)[2].split(',');
+                    for (var i = 0; i < params.length; i++) {
+                        var param = params[i];
+                        condition = condition.replace(param, 'this.' + param);
+                    }
+                }
+            }
+
+            function isFunction(prop) {
+                return /(.*)\((.*)+\)$/.test(prop);
+            }
+
+            var result = jsEval(condition, scope);
+
+            console.log(condition);
+
+            if (result === false) {
+                $el.css('display', 'none');
             }
         }
     }
+
+    function isLiteral(expr) {
+        try {
+            var evaled = eval(expr);
+
+            return (typeof evaled).indexOf['object', 'function'] != -1;
+        } catch (e) {
+            return false;
+        }
+    }
+
+    function jsEval(expr, scope) {
+        return function(expr) {
+            return eval(expr);
+        }.call(scope, expr);
+    }
+
+    function setVariables($el, scope) {
+        var html = elToHtml($el);
+        var variables = html.match(/{(.*?)}/g);
+
+        var replaced = false;
+
+        for (var i = 0; i < variables.length; i++) {
+            var symbol = variables[i];
+            value = scope[symbol.replace(/{|}/g, '')];
+            if (value) {
+                html = html.replace(variables[i], value);
+                replaced = true;
+            }
+        }
+
+        var $newElement = false;
+        if (replaced) {
+            $newElement = $(html).replaceAll($el);
+        }
+
+        return $newElement ? $newElement : $el;
+    }
+
+    function hasIf($el) {
+        return $el[0].hasAttribute('kIf');
+    }
+
+    function hasVariables($el) {
+        var html = elToHtml($el);
+        return /{.*}/.test(html);
+    }
+
+    function elToHtml($el) {
+        return $('<div>').append($el.clone()).html();
+    }
+
+    function hasFor($el) {
+        return $el[0].hasAttribute('kFor');
+    }
+
+    function isForCompiled($el) {
+        return $el[0].hasAttribute('kFor-compiled');
+    }
+
+    function applyFor(indexName, items, $el, scope) {
+        var newScope = new komp.Scope(scope);
+
+        $el.attr('kFor-compiled', true);
+
+        for (var i = 0; i < items.length; i++) {
+            newScope[indexName] = items[i];
+
+            var $newEl = $el.clone();
+
+            this.eval($newEl, newScope);
+
+            var t = this;
+            $newEl.children().each(function(i, childEl) {
+                t.eval($(childEl), newScope);
+            })
+            
+            console.log(elToHtml($newEl));
+            
+            $newEl.insertBefore($el);
+
+        }
+
+        $el.remove();
+    }
+
 }
